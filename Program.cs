9b3b3b64e8ba496fff13f1fcab6eb02cbaade162
khumbelon <163254485+khumbelon@users.noreply.github.com>
2026-09-08 20:24:27 +0200

    upload program.cs file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CyberSecurityAwarenessBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoiceGreeting voice = new VoiceGreeting();
+            voice.Play();
+
+            AsciiArt art = new AsciiArt();
+            art.Display();
+
+            Chatbot bot = new Chatbot();
+            bot.Start();
+
+        }
+    }
+}
