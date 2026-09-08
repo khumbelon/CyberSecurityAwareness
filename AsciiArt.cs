eaa5af1064f734f72adc05fcb40919a54ddc7a9a
khumbelon <163254485+khumbelon@users.noreply.github.com>
2026-09-08 20:22:19 +0200

    add AsciiArt.cs file

---
+++ AsciiArt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CyberSecurityAwarenessBot
+{
+    public class AsciiArt
+    {
+        public void Display()
+        {
+            DisplayLogo();
+        }
+
+        public void DisplayLogo()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("======================================");
+            Console.WriteLine("     CYBERSECURITY AWARENESS BOT");
+            Console.WriteLine("======================================");
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome to the Cybersecurity Awareness Chatbot!");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.WriteLine(@"__   __                               
+__  _  __ ____ |  | |  |   ____   ____   _____   ____  
+\ \/ \/ // __ \|  | |  | _/ ___\ / __ \ /     \_/ __ \ 
+ \     /\  ___/_  |__  |__  \___(  \_\ )  | |  \  ___/_
+  \/\_/  \___  /____/____/\___  /\____/|__|_|  /\___  /
+             \/               \/             \/     \/ ");
+
+            Console.Write("You > ");
+        }
+    }
+}
