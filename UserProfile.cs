798e64bb956edd86d0e0ea88f32c5dbb1e7ca83f
khumbelon <163254485+khumbelon@users.noreply.github.com>
2026-09-08 20:25:56 +0200

    add userprofile file

---
+++ UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;// 
+
+
+namespace CyberSecurityAwarenessBot // This namespace contains classes related to the CyberSecurityAwarenessBot application
+{
+    public class UserProfile
+    {
+        public string Name { get; set; } // User's name
+        public string FavoriteTopic { get; set; } // User's favorite cybersecurity topic
+        = string.Empty;
+
+    }
+}
