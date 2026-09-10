e72d5de6887b8cc5a8f7a50ffe93f32c51f51cdd
khumbelon <163254485+khumbelon@users.noreply.github.com>
2026-09-10 13:03:05 +0200

    Add comments to Programs for better clarity and comments
    Added comments to clarify the purpose of each section of the code.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
-﻿using System;
+﻿using System;//imports system namespace for console input and output
 
-namespace CyberSecurityAwarenessBot
+namespace CyberSecurityAwarenessBot// name of the project and namespace
 {
-    class Program
+    class Program //declares the program that starts the chatbot application
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // The main entry point of the application
         {
-            VoiceGreeting voice = new VoiceGreeting();
+            VoiceGreeting voice = new VoiceGreeting();//creates an instance of the VoiceGreeting class
             voice.Play();
 
-            AsciiArt art = new AsciiArt();
+            AsciiArt art = new AsciiArt();//creates an instance of the AsciiArt class
             art.Display();
 
-            Chatbot bot = new Chatbot();
+            Chatbot bot = new Chatbot();//creates an instance of the Chatbot class
             bot.Start();
 
         }
     }
 }
+
