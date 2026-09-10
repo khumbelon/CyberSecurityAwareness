b6acf46771b6d6fb9197e5710b31bdb35d021e24
khumbelon <163254485+khumbelon@users.noreply.github.com>
2026-09-10 13:00:52 +0200

    Refactor UserProfile class structure and comments

--- UserProfile.cs
+++ UserProfile.cs
@@ -1,9 +1,9 @@
-﻿using System;// 
+using System;// imports system namespace for console input and output
 
 
 namespace CyberSecurityAwarenessBot // This namespace contains classes related to the CyberSecurityAwarenessBot application
 {
-    public class UserProfile
+    public class UserProfile // This class represents the user's profile information
     {
         public string Name { get; set; } // User's name
         public string FavoriteTopic { get; set; } // User's favorite cybersecurity topic
